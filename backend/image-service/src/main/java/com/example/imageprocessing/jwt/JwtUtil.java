afacf8081484cb061ce5c687aca0a4ed4b775396
Shubham Vashishtha <shubhvash87@gmail.com>
2025-06-27 23:38:41 +0530

    refactor

--- backend/image-service/src/main/java/com/example/imageprocessing/jwt/JwtUtil.java
+++ backend/image-service/src/main/java/com/example/imageprocessing/jwt/JwtUtil.java
@@ -3,10 +3,12 @@ package com.example.imageprocessing.jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
     private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
